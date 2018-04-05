78baae8c93a257f173c2a2ebe97eb7ac01ea82fa
Hao Wu <hao.wu.fudan@gmail.com>
2018-04-05 18:56:01 -0400

    - wrote wrapper for digital output and counter frequency pulses output  - wrote wrapper for 2 motor tracking system

---
+++ AntCamHW/daqmotor/daq_do_dev.py
@@ -0,0 +1,182 @@
+from PyDAQmx import *
+from ctypes import byref, c_ulong,c_int32
+import numpy as np
+
+class DAQSimpleDOTask(Task):
+    '''
+    a simple task that set one digital output line to high or low
+    '''
+    
+    def __init__(self,chan= 'Dev2/port0/line5'):
+        '''
+        chan: name of the chanel, in the format of Dev2/port0/line0
+        '''
+        Task.__init__(self)
+        self.chan = chan
+        self.CreateDOChan(self.chan,'',DAQmx_Val_ChanPerLine)
+        
+    def write(self,value,timeout = 0.0001):
+        '''
+        write a single sample to the digital line
+        
+        value: 0=low 1=high
+        timeout: timeout in seconds
+        '''
+        written = c_int32(0)
+        self.WriteDigitalLines(1,True,timeout,DAQmx_Val_GroupByScanNumber,value,byref(written),None)
+        
+    def high(self,timeout = 0.0001):
+        '''
+        change the digital line to high
+        
+        timeout: timeout in seconds
+        '''
+        self.write(1,timeout)
+        
+    def low(self,timeout =0.0001):
+        '''
+        change the digital line to low
+        
+        timeout: timeout in seconds
+        '''
+        self.write(0,timeout)
+
+    def start(self):
+        '''
+        start task
+        '''
+        self.StartTask()
+        
+    def stop(self):
+        '''
+        stop task
+        '''
+        self.StopTask()
+        
+    def close(self):
+        '''
+        close task
+        '''
+        self.ClearTask()
+        
+class DAQContDOTask(Task):
+    '''
+    a task that set one digital output to high/low for a selected period of time
+    '''
+    
+    def __init__(self,chan= 'Dev2/port0/line5', rate = 1000):
+        '''
+        chan: name of the chanel, in the format of Dev2/port0/line0
+        rate: sampling rate in Hz
+        '''
+        Task.__init__(self)
+        self.chan = chan
+        self.rate = rate
+        
+        self.CreateDOChan(self.chan,'',DAQmx_Val_ChanPerLine)
+        self.CfgSampClkTiming('',self.rate,DAQmx_Val_Rising,DAQmx_Val_ContSamps,1000)
+
+        
+    def write(self,value):
+        '''
+        write a single sample to the digital line
+        '''
+        val_array = np.zeros((10,),dtype = np.uint8)
+        val_array[:] = value
+
+        self.WriteDigitalLines(1,False,0,DAQmx_Val_GroupByScanNumber,val_array,byref(written),None)
+        
+    def high(self):
+        '''
+        change the digital line to high
+        '''
+        self.write(1)
+        
+    def start(self):
+        '''
+        start task
+        '''
+        self.StartTask()
+        
+    def done(self):
+        '''
+        returns if a started task is done outputting
+        if done, returns True
+        '''
+        value = c_ulong(False)
+        self.IsTaskDone(byref(value))
+        return bool(value)
+        
+    def stop(self):
+        '''
+        stop task
+        '''
+        self.StopTask()
+        
+    def low(self):
+        '''
+        change the digital line to low
+        '''
+        self.write(0)
+        
+    def close(self):
+        '''
+        close task
+        '''
+        self.ClearTask()
+        
+class DAQCOTask(Task):
+    '''
+    This is a task where you can generate precisely time pulses with a certain frequency
+    '''
+    
+    def __init__(self,counter = 'Dev2/ctr0',term = '/Dev2/PFI12',freq = 4000,dc = 0.5):
+        '''
+        Initialize the task
+        
+        counter: name of the counter used, e.g. Dev2/ctr0
+        term: name of terminal used, e.g. /Dev2/PFI10, note that the first slash has to be there
+        freq: frequency in Hz
+        dc: pulse duty cycle of the high period, from 0.0 - 1.0
+        '''
+        Task.__init__(self)
+        self.counter = counter
+        self.term = term
+        
+        self.CreateCOPulseChanFreq(self.counter,'',DAQmx_Val_Hz,DAQmx_Val_Low,0,freq,dc)
+        self.SetCOPulseTerm(self.counter, self.term)
+        
+    def set_pulses(self,num_pulses = 800):
+        '''
+        Set the number of pulses to be generated for the next sample
+        
+        num_pulses: number of pulses to output
+        '''
+        self.CfgImplicitTiming(DAQmx_Val_FiniteSamps,num_pulses)
+        
+    def start(self):
+        '''
+        start task
+        '''
+        self.StartTask()
+        
+    def done(self):
+        '''
+        returns if a started task is done outputting
+        if done, returns True
+        '''
+        value = c_ulong(False)
+        self.IsTaskDone(byref(value))
+        return bool(value)
+        
+    def stop(self):
+        '''
+        stop task
+        '''
+        self.StopTask()
+
+    def close(self):
+        '''
+        close task
+        '''
+        self.ClearTask()
---
+++ AntCamHW/daqmotor/daqmotor_dev.py
@@ -0,0 +1,108 @@
+'''
+Created on Apr 5, 2018
+
+@author: Hao Wu
+'''
+from daq_do_dev import DAQContDOTask,DAQCOTask,DAQSimpleDOTask
+import numpy as np
+
+def transform_cord(old_cord, angle = 45):
+    theta = np.radians(angle)
+    c, s = np.cos(theta), np.sin(theta)
+    R = np.matrix([[c, -s], [s, c]])
+    new_cord = np.array(np.matmul(R,old_cord).astype(np.int32)).squeeze()
+    direction = (new_cord>0).astype(np.uint8)
+
+    return direction,np.abs(new_cord)
+    
+
+class DAQMotorDev(object):
+    '''
+    This the device wrapper for stepper motor driver controlled by NIDAQ
+    '''
+
+
+    def __init__(self,chans,counter,term,freq,dc):
+        '''
+        This initialize the device
+        
+        ena_chan: name of the enable channel
+        dir_chan: name of the direction channel
+        counter: name of the counter
+        term: name of the PFI terminal
+        freq: frequency(Hz) of the pulse
+        dc: duty cycle of the pulse, 0.0 - 1.0
+        '''
+        self.chans = chans
+        self.counter = counter
+        self.term = term
+        self.freq = freq
+        self.dc = dc
+        
+        self.do_task = DAQSimpleDOTask(self.chans)
+        self.co_task1 = DAQCOTask(self.counter[0],self.term[0],self.freq,self.dc)
+        self.co_task2 = DAQCOTask(self.counter[1],self.term[1],self.freq,self.dc)
+
+    def move_manual(self,direction_id,num_pulses = [100,100]):
+        '''
+        Move in a selected direction for number of steps
+        direction_id: 0 = left, 1 = right, 2 = up, 3 = down
+        num_pulses: two integer number for 
+        '''
+
+        directions = []
+        directions.append(np.array([0,0,0,0],dtype = np.uint8))
+        directions.append(np.array([0,0,1,1],dtype = np.uint8))
+        directions.append(np.array([0,0,1,0],dtype = np.uint8))
+        directions.append(np.array([0,0,0,1],dtype = np.uint8))
+        
+        self.do_task.write(directions[direction_id])
+        self.send_pulses(num_pulses)
+        
+    def send_pulses(self,num_pulses = [100,100]):
+        self.co_task1.set_pulses(num_pulses[0])
+        self.co_task2.set_pulses(num_pulses[1])
+        if not num_pulses[0] == 0:
+            self.co_task1.start()
+        if not num_pulses[1] == 0:
+            self.co_task2.start()
+        while not self.done():
+            pass
+        if not num_pulses[0] == 0:
+            self.co_task2.stop()
+        if not num_pulses[1] == 0:
+            self.co_task2.stop()
+        
+    def move(self,direction_signs,num_pulses):
+        direction = np.zeros((4,),dtype = np.uint8)
+        direction[2:4] = direction_signs
+        print(direction)
+        self.do_task.write(direction)
+        self.send_pulses(num_pulses)
+        
+    def move_cartesian(self,num_pulses,angle = 45):
+        old_cord = np.array(num_pulses)
+        old_cord[1] *= -1
+        direction, new_pulses = transform_cord(old_cord,angle)
+        self.move(direction,new_pulses)
+
+    def done(self):
+        return (self.co_task1.done() and self.co_task2.done())
+    
+    def close(self):
+        self.do_task.close()
+        self.co_task1.close()
+        self.co_task2.close()
+        
+if __name__ == '__main__':
+    motor = DAQMotorDev(chans ='Dev2/port0/line5,Dev2/port1/line0,Dev2/port0/line1,Dev2/port1/line1',
+                        counter = ['Dev2/ctr0','Dev2/ctr1'],
+                        term =  ['/Dev2/PFI12','/Dev2/PFI13'],
+                        freq = 4000,
+                        dc = 0.5)
+    motor.move_cartesian([0,800], 45)
+
+    motor.close()
+    
+        
+        
