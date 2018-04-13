4b02cfb06eb50dddc59e8058174b21445a1341d1
Hao Wu <hao.wu.fudan@gmail.com>
2018-04-13 14:04:10 -0400

    -added splashscreen and modified UI layout

---
+++ RunAntCamQuiet.bat
@@ -0,0 +1,1 @@
+quiet RunAntCam.bat
--- main_interface.py
+++ main_interface.py
@@ -1,10 +1,10 @@
-'''
+"Create Measurement objects"'''
 Created on Mar 26, 2018
 
 @author: Hao Wu
 '''
 from ScopeFoundry import BaseMicroscopeApp
-from qtpy import QtGui
+from qtpy import QtGui, QtWidgets
 
 class AntCamApp(BaseMicroscopeApp):
 
@@ -15,14 +15,22 @@ class AntCamApp(BaseMicroscopeApp):
     # You must define a setup function that adds all the 
     #capablities of the microscope and sets default settings
     def setup(self):
+        #start splash screen
+        splash_pix = QtGui.QPixmap('splash.png')
+        splash = QtWidgets.QSplashScreen(splash_pix)
+        splash.show()
+        splash.setEnabled(False)
         
         #Add App wide settings
         
         #Add hardware components
+        splash.showMessage("Adding Hardware Components...")
         print("Adding Hardware Components")
         from AntCamHW.flircam.flircam_hw import FLIRCamHW
+        splash.showMessage("Adding Tracking Camera...")
         track_cam = FLIRCamHW(self)
         track_cam.settings.camera_sn.update_value('16130612')
+        splash.showMessage("Adding Side Camera...")
         track_cam.name = 'track_cam'
         wide_cam = FLIRCamHW(self)
         wide_cam.settings.camera_sn.update_value('15188107')
@@ -39,6 +47,7 @@ class AntCamApp(BaseMicroscopeApp):
         #self.add_hardware(DAQTimerHW(self))
 
         #Add measurement components
+        splash.showMessage("Create Measurement objects...")
         print("Create Measurement objects")
         from AntCamMS.ant_watch import AntWatchMeasure
         self.add_measurement(AntWatchMeasure(self))
@@ -47,6 +56,7 @@ class AntCamApp(BaseMicroscopeApp):
         # load side panel UI
         
         # show ui
+        splash.finish(self.ui)
         self.ui.show()
         self.ui.activateWindow()
     
@@ -57,7 +67,9 @@ if __name__ == '__main__':
 
     myappid = 'MurthyLab.PyLab.AntCam.v1.0.0' # arbitrary string
     ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
+
     app = AntCamApp(sys.argv)
+    
     app.ui.setWindowIcon(QtGui.QIcon('ant_icon.ico'))
     app.hardware['track_cam'].connected.update_value(True)
     app.hardware['wide_cam'].connected.update_value(True)
